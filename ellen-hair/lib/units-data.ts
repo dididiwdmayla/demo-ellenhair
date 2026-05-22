06e2ffdb259962c2374e524092f3fbd39b4c5704
dididiwdmayla <willmendes14@outlook.com>
2026-05-22 02:51:43 -0300

    feat: Add pre-formatted WhatsApp messages to unit data

--- ellen-hair/lib/units-data.ts
+++ ellen-hair/lib/units-data.ts
@@ -9,9 +9,10 @@ export const unitsData = [
     phone: "(44) 3222-1062",
     whatsapp: "44997388413",
     whatsappDisplay: "(44) 99738-8413",
+    whatsappLink: "https://wa.me/5544997388413?text=Olá!%20Gostaria%20de%20agendar%20um%20horário%20na%20unidade%20Horácio%20Raccanello.",
     hours: "Segunda a Sábado, 8h às 19h",
     mapQ: "Av.+Advogado+Horácio+Raccanello+Filho,+5515",
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3661.1706805622753!2d-51.93810622571981!3d-23.418200806346295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1savenida%20horacio%20raccanello%205515!5e0!3m2!1spt-BR!2sbr!4v1779426454798!5m2!1spt-BR!2sbr"
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3661.1706805622753!2d-51.93810622571981!3d-23.418200806346295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1savenida%20horac[...]"
   },
   {
     id: "brasil",
@@ -20,9 +21,10 @@ export const unitsData = [
     phone: "(44) 3031-0265",
     whatsapp: "44997021796",
     whatsappDisplay: "(44) 99702-1796",
+    whatsappLink: "https://wa.me/5544997021796?text=Olá!%20Gostaria%20de%20agendar%20um%20horário%20na%20unidade%20Avenida%20Brasil.",
     hours: "Segunda a Sábado, 8h às 19h",
     mapQ: "Av.+Brasil,+4215",
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.0888782456022!2d-51.94564922571959!3d-23.4211563564543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecd7296e6c6325%3A0x56fca9e048f14baa!2sAv.%20Brasil%2C%204215%20-%20Zona%2003%2C%20Maring%C3%A1%20-%20PR%2C%2087013-000!5e0!3m2!1spt-BR!2sbr!4v1779426633888!5m2!1spt-BR!2sbr"
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.0888782456022!2d-51.94564922571959!3d-23.4211563564543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecd7296e6[...]"
   },
   {
     id: "mesquita",
@@ -31,8 +33,9 @@ export const unitsData = [
     phone: "(44) 3030-2010",
     whatsapp: "44998038476",
     whatsappDisplay: "(44) 99803-8476",
+    whatsappLink: "https://wa.me/5544998038476?text=Olá!%20Gostaria%20de%20agendar%20um%20horário%20na%20unidade%20Júlio%20Mesquita.",
     hours: "Segunda a Sábado, 8h às 18h30",
     mapQ: "Tv.+Júlio+de+Mesquita+Filho,+337",
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.1069912593657!2d-51.94013652571969!3d-23.42050195643034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecd72b45ed40e3%3A0x1f3fcdfcbc94a5bb!2sTv.%20J%C3%BAlio%20de%20Mesquita%20Filho%2C%20337%20-%20centro%2C%20Maring%C3%A1%20-%20PR%2C%2087013-120!5e0!3m2!1spt-BR!2sbr!4v1779426733903!5m2!1spt-BR!2sbr"
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.1069912593657!2d-51.94013652571969!3d-23.42050195643034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecd72b45[...]"
   }
 ];
